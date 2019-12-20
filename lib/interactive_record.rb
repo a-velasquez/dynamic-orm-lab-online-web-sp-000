437719cb475d676cb96e4598a32454203af2fef0
Alex Velasquez <jtinnesco@gmail.com>
2019-12-20 06:37:43 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,8 +54,7 @@ class InteractiveRecord
   end
 
   def self.find_by(student_hash)
-    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{student_hash.key[0].to_s} = #{student_hash.values[0].to_s}")
-
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{student_hash.key[0].to_s} = '#{student_hash.values[0].to_s}'")
   end
 
 end
